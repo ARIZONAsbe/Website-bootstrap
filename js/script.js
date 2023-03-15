011cf0e3a8a6496be0d60f499aa204145f83cffa
Krystian Kamiński <krystian.kam@protonmail.com>
2023-03-15 20:47:30 +0100

    made navbar responsible

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const nav = document.querySelector(".navbar");
+  const btnBurger = document.querySelector(".navbar-toggler");
+  const allNavItems = document.querySelectorAll(".nav-link");
+  const navList = document.querySelector(".navbar-collapse");
 
   function addShadow() {
-    if (window.scrollY >= 300) {
+    if (window.scrollY >= 40) {
       nav.classList.add("shadow-bg");
     } else {
       nav.classList.remove("shadow-bg");
     }
   }
+
+  allNavItems.forEach((item) =>
+    item.addEventListener("click", () => navList.classList.remove("show"))
+  );
 
   window.addEventListener("scroll", addShadow);
 });
